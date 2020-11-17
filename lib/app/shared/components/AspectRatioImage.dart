90c4edef6583080e87e88527552d4a7e27e901a9
Felipe Nobre <felipe@parentscheme.com>
2020-11-17 17:03:34 -0300

    Feat : Create AspectRatioImage component

---
+++ lib/app/shared/components/AspectRatioImage.dart
@@ -0,0 +1,46 @@
+import 'package:clubbers/app/shared/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+
+/*
+  APECT RATIO IMAGE COMPONENT
+  This component draws an image with rounded border
+  and can have a custom aspect ratio
+
+
+  @param ratio
+  @aparam image
+
+*/
+
+class AspectRatioImage extends StatelessWidget {
+  final double ratio;
+  final String image;
+
+  const AspectRatioImage({this.ratio, this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(1),
+            spreadRadius: 0,
+            blurRadius: 20,
+            offset: Offset(0, 0),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: AppStyles.borderRadius15,
+        child: AspectRatio(
+          aspectRatio: ratio,
+          child: Image(
+            image: AssetImage(image),
+            fit: BoxFit.cover, // use this
+          ),
+        ),
+      ),
+    );
+  }
+}
