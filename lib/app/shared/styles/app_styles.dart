2c2a28c10acc7c65aec46094b7deea7bdf9cb914
Felipe Nobre <felipe@parentscheme.com>
2020-11-17 13:37:31 -0300

    Feat : Updated spacing naming

--- lib/app/shared/styles/app_styles.dart
+++ lib/app/shared/styles/app_styles.dart
@@ -7,42 +7,58 @@ class AppStyles {
   static const Color primaryColor = Color.fromRGBO(252, 18, 103, 1);
   static const Color secondaryColor = Color.fromRGBO(187, 107, 217, 1);
   static const Color bodyColor = Color.fromRGBO(46, 58, 89, 1);
+  static const Gradient primaryGradient =
+      LinearGradient(colors: [secondaryColor, primaryColor]);
 
+  /*
+    DEFAULT SPACING
+  */
+  static const spacing_small = 8.0;
+  static const spacing_normal = 16.0;
+  static const spacing_big = 24.0;
+  static const spacing_extra = 32.0;
   /*
     HORIZONTAL SPACING
   */
   static const EdgeInsets horizintalSpacing =
-      EdgeInsets.symmetric(horizontal: 16.0);
+      EdgeInsets.symmetric(horizontal: spacing_normal);
   static const EdgeInsets horizintalSpacingSmall =
-      EdgeInsets.symmetric(horizontal: 8.0);
+      EdgeInsets.symmetric(horizontal: spacing_small);
   static const EdgeInsets horizintalSpacingBig =
-      EdgeInsets.symmetric(horizontal: 24.0);
+      EdgeInsets.symmetric(horizontal: spacing_big);
 
   /*
     VERTICAL SPACING
   */
   static const EdgeInsets verticalSpacing =
-      EdgeInsets.symmetric(vertical: 16.0);
+      EdgeInsets.symmetric(vertical: spacing_normal);
   static const EdgeInsets verticalSpacingSmall =
-      EdgeInsets.symmetric(vertical: 8.0);
+      EdgeInsets.symmetric(vertical: spacing_small);
   static const EdgeInsets verticalSpacingBig =
-      EdgeInsets.symmetric(vertical: 24.0);
+      EdgeInsets.symmetric(vertical: spacing_big);
 
   /*
     HORIZONTAL & VERTICAL SPACING
   */
-  static const EdgeInsets spacing = EdgeInsets.all(16.0);
-  static const EdgeInsets spacingSmall = EdgeInsets.all(8.0);
-  static const EdgeInsets spacingBig = EdgeInsets.all(24.0);
-  static const EdgeInsets spacingExtra = EdgeInsets.all(32.0);
+  static const EdgeInsets spacingNormal = EdgeInsets.all(spacing_normal);
+  static const EdgeInsets spacingSmall = EdgeInsets.all(spacing_small);
+  static const EdgeInsets spacingBig = EdgeInsets.all(spacing_big);
+  static const EdgeInsets spacingExtra = EdgeInsets.all(spacing_extra);
 
   /*
     BOX SHADOW
   */
   static BoxShadow shadowBoxDecoration = BoxShadow(
-    color: Colors.grey.withOpacity(0.2),
+    color: Colors.grey.withOpacity(0.25),
     spreadRadius: 0,
     blurRadius: 20,
+    offset: Offset(0, 0),
+  );
+
+  static BoxShadow shadowBoxDecorationForButtons = BoxShadow(
+    color: Colors.grey.withOpacity(0.8),
+    spreadRadius: 0,
+    blurRadius: 8,
     offset: Offset(0, 0),
   );
 
