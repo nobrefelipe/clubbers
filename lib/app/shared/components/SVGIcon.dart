c0724ce0993ba47fed3313e8d6944c3060c1cd1d
Felipe Nobre <felipe@parentscheme.com>
2020-11-17 11:12:51 -0300

    Feat :  Added active status to SVGIcon

--- lib/app/shared/components/SVGIcon.dart
+++ lib/app/shared/components/SVGIcon.dart
@@ -1,9 +1,15 @@
+import 'package:clubbers/app/shared/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 /*
-  This widget draws an custom icon from a svg 
+  This widget draws a custom icon from a svg 
   It receives the icon name and color as parameters.
+  It can also receive an $active parameter to indentify if
+  the icon is has active status (usefull for routes or tabs)
+
+  By defualt $active is set to false.
+  Is $color is passed, the $active parameter will be ignored.
 
   The icons are located at ./lib/assets/svg
 
@@ -15,15 +21,26 @@ import 'package:flutter_svg/svg.dart';
 class SVGIcon extends StatelessWidget {
   final String icon;
   final Color color;
-  SVGIcon({this.icon, this.color});
+  final bool active;
+  SVGIcon({this.icon, this.color, this.active = false});
 
   final String path = "./lib/assets/svg";
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
+      // path to the svg icon
       '$path/$icon.svg',
-      color: color,
+      // if a color is passed
+      color: color != null
+          // set color passed from parent
+          ? color
+          // otherwise if active is set
+          : active
+              // set icon color with the primary color
+              ? AppStyles.primaryColor
+              // otherwise set it with opaque color
+              : AppStyles.bodyColor.withOpacity(0.5),
       matchTextDirection: true,
     );
   }
