d7dd57db55bd526c7a33405a849f4f0c4acdf658
Felipe Nobre <felipe@parentscheme.com>
2020-11-19 14:55:20 -0300

    Feat : Add feature to accpet custom svg icons

--- lib/app/shared/components/TitleWithIcon.dart
+++ lib/app/shared/components/TitleWithIcon.dart
@@ -1,3 +1,4 @@
+import 'package:clubbers/app/shared/components/SVGIcon.dart';
 import 'package:clubbers/app/shared/styles/app_styles.dart';
 import 'package:clubbers/app/shared/styles/typography.dart';
 import 'package:flutter/material.dart';
@@ -6,28 +7,43 @@ import 'package:flutter_icons/flutter_icons.dart';
 /*
   TITLE WITH ICON COMPONENT
   This component draws a title with an icon 
-  placed on its left side
+  placed on its left side.
+
+  We can set a custom icon by passing the name of the icon
+  located in /assets/svg/. Using SVGIcon component
+  
+  If customIcon is set we use it, otherwise we 
+  get the icon the parameter
 
   @param icon
   @param title
+  @param customIcon
 
 */
 
 class TitleWithIcon extends StatelessWidget {
   final IconData icon;
   final String title;
+  final String customIcon;
 
-  const TitleWithIcon({this.icon, this.title});
+  const TitleWithIcon({this.icon, this.title, this.customIcon});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Icon(
-          icon,
-          color: AppStyles.primaryColor,
-          size: 18.0,
-        ),
+        customIcon != null
+            ? SVGIcon(
+                icon: customIcon,
+                active: true,
+                scale: 1.15,
+              )
+            : Icon(
+                icon,
+                color: AppStyles.primaryColor,
+                size: 18.0,
+              ),
         VerticalDivider(width: 4),
         Heading(
           text: title,
