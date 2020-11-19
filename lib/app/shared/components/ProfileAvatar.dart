30285137751eeb458f586288d37ae95da0380548
Felipe Nobre <felipe@parentscheme.com>
2020-11-19 14:07:46 -0300

    Feat : Add border control

--- lib/app/shared/components/ProfileAvatar.dart
+++ lib/app/shared/components/ProfileAvatar.dart
@@ -2,10 +2,23 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:clubbers/app/shared/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 
+/*
+  PROFILE AVATAR COMPONENT
+  This component is used for profile avatars or rounded images
+
+  Its made of two CircleAvatar widgets. 
+  To allow us to have a border around the image.
+
+  @param image
+  @param hasBorder
+
+*/
+
 class ProfileAvatar extends StatelessWidget {
   final String image;
+  final bool hasBorder;
 
-  const ProfileAvatar(this.image);
+  const ProfileAvatar({this.image, this.hasBorder = false});
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +28,7 @@ class ProfileAvatar extends StatelessWidget {
           radius: 20.0,
           backgroundColor: AppStyles.primaryColor,
           child: CircleAvatar(
-            radius: 19.0,
+            radius: hasBorder ? 19.0 : 20.0,
             backgroundColor: Colors.grey[200],
             backgroundImage: CachedNetworkImageProvider(image),
           ),
