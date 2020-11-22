ec56587138eb6e623411558b7c1f1aac121b7b2b
Felipe Nobre <felipe@parentscheme.com>
2020-11-22 15:09:19 -0300

    Feat : Show modal when user finalises the signup process

--- lib/app/modules/auth/signup/pages/perfil.dart
+++ lib/app/modules/auth/signup/pages/perfil.dart
@@ -5,6 +5,7 @@ import 'package:clubbers/app/shared/components/logo.dart';
 import 'package:clubbers/app/shared/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../signup_controller.dart';
@@ -44,121 +45,131 @@ class _PerfilPageState extends ModularState<PerfilPage, SignupController> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.only(top: screenSize.height / 7),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //
-              // Page Title
-              //
-              Text(
-                "Foto de perfil \ne nome completo",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.comfortaa(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: AppStyles.primaryColor,
-                  letterSpacing: -0.8,
-                ),
-              ),
-              SizedBox(height: 20),
-              //
-              // Login Box
-              //
-              ShadowContainer(
-                margin: EdgeInsets.symmetric(
-                  horizontal: AppStyles.spacing_big,
-                  vertical: AppStyles.spacing_normal,
-                ),
+          /*
+            OBSERVER
+            Observe for changes in any @observable in the controller
+            so we update the view when those change
 
-                //
-                // Input Fields
-                //
-                child: Container(
-                  height: 280,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      //
-                      // Description
-                      //
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Text(
-                          "Toque para editar",
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      //
-                      // Profile Image
-                      //
-                      GestureDetector(
-                        onTap: () => print("take picture"),
-                        child: Container(
-                          width: 90,
-                          height: 90,
-                          margin:
-                              EdgeInsets.only(bottom: AppStyles.spacing_big),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(FontAwesome.picture_o),
-                        ),
-                      ),
-                      //
-                      // Full name
-                      //
-                      FormInput(
-                        fieldName: "Nome Completo",
-                        textInputAction: TextInputAction.go,
-                        keyboardType: TextInputType.text,
-                        onSubmitted: (value) =>
-                            Modular.to.pushNamedAndRemoveUntil(
-                          '/home',
-                          (Route route) => false,
-                        ),
-                        onChanged: (value) => controller.getName(value),
-                      ),
-                      SizedBox(height: 20),
-                    ],
+            As this view is the last step in the registration flow
+            we observe controller.loading to change to give feedbacl to the user
+          */
+          child: Observer(
+            builder: (_) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //
+                  // Page Title
+                  //
+                  Text(
+                    "Foto de perfil \ne nome completo",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.comfortaa(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: AppStyles.primaryColor,
+                      letterSpacing: -0.8,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-              //
-              // CTA
-              //
-              Padding(
-                padding: const EdgeInsets.only(left: 64.0, right: 8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    //
-                    //  Go back
-                    //
-                    GestureDetector(
-                      onTap: () => Modular.to.pop(),
-                      child: Text('Voltar'),
+                  SizedBox(height: 20),
+                  //
+                  // Login Box
+                  //
+                  ShadowContainer(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: AppStyles.spacing_big,
+                      vertical: AppStyles.spacing_normal,
                     ),
+
                     //
-                    //  Next step
+                    // Input Fields
                     //
-                    BUTTON(
-                      text: "Finalizar",
-                      width: 160,
-                      icon: AntDesign.arrowright,
-                      onPressed: () => Modular.to.pushNamedAndRemoveUntil(
-                        '/home',
-                        (Route route) => false,
+                    child: Container(
+                      height: 280,
+                      width: double.infinity,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          //
+                          // Description
+                          //
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Text(
+                              "Toque para editar",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          //
+                          // Profile Image
+                          //
+                          GestureDetector(
+                            onTap: () => print("take picture"),
+                            child: Container(
+                              width: 90,
+                              height: 90,
+                              margin: EdgeInsets.only(
+                                  bottom: AppStyles.spacing_big),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[300],
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(FontAwesome.picture_o),
+                            ),
+                          ),
+                          //
+                          // Full name
+                          //
+                          FormInput(
+                            fieldName: "Nome Completo",
+                            textInputAction: TextInputAction.go,
+                            keyboardType: TextInputType.text,
+                            onSubmitted: (value) => controller.signup(context),
+                            onChanged: (value) => controller.getName(value),
+                          ),
+                          SizedBox(height: 20),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-            ],
+                  ),
+                  SizedBox(height: 20),
+
+                  //
+                  // CTA
+                  //
+                  Padding(
+                    padding: const EdgeInsets.only(left: 64.0, right: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        //
+                        //  Go back
+                        //
+                        GestureDetector(
+                          onTap: () => Modular.to.pop(),
+                          child: Text('Voltar'),
+                        ),
+                        //
+                        //  Next step
+                        //
+                        // aqui precisa do build block pra escurar mudancas no contorller
+                        // quando clicar em finalizar tavez mostrar uma modal com um bom feedback
+                        // quando terminar de criar o usuario a modal continua com umamesnsagem de bem vindo
+                        // personalizada e um botao de ir acessar o app
+                        BUTTON(
+                          text: "Finalizar",
+                          width: 160,
+                          icon: AntDesign.arrowright,
+                          onPressed: () => controller.signup(context),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
--- lib/app/modules/auth/signup/signup_controller.dart
+++ lib/app/modules/auth/signup/signup_controller.dart
@@ -1,3 +1,9 @@
+import 'package:clubbers/app/api/mock/users.dart';
+import 'package:clubbers/app/shared/components/Modal.dart';
+import 'package:clubbers/app/shared/components/Spinner.dart';
+import 'package:clubbers/app/shared/styles/typography.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:mobx/mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -67,5 +73,47 @@ abstract class _SignupControllerBase with Store {
         ? preferences.remove(key)
         : preferences.add(key);
     print(preferences);
+  }
+
+  //
+  // SIGNUP
+  //
+
+  @action
+  Future<void> signup(context) async {
+    //
+    //  SHOW FEED BACK MODAL
+    //
+    Navigator.of(context).push(
+      MODAL(
+        title: "Quase la!",
+        content: Column(
+          children: [
+            TEXT(
+              textAlign: TextAlign.center,
+              text:
+                  "Estamos criando a tua conta Estamos criando a tua conta Estamos criando a tua conta Estamos criando a tua conta  criando a tua conta",
+            ),
+            SizedBox(height: 20),
+            Spinner(icon: FontAwesome.spinner),
+          ],
+        ),
+      ),
+    );
+
+    //
+    // CREATE THE USER
+    //
+    Future.delayed(Duration(seconds: 5), () {
+      // Create the user
+      currentUser = users.first;
+
+      // If we got a user, navigate to the home page
+      if (currentUser != null)
+        Modular.to.pushNamedAndRemoveUntil(
+          '/home',
+          (Route route) => false,
+        );
+    });
   }
 }
