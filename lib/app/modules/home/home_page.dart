5d07ec7fc7d55c483c37cf370eeafc4af1fb2ae1
Felipe Nobre <felipe@parentscheme.com>
2020-11-22 22:54:08 -0300

    Feat :  Create Home Screen

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,6 +1,15 @@
+import 'package:clubbers/app/api/mock/locals.dart';
+import 'package:clubbers/app/api/mock/users.dart';
+import 'package:clubbers/app/shared/components/AppBottomBar.dart';
+import 'package:clubbers/app/shared/components/FavoritePlaces.dart';
+import 'package:clubbers/app/shared/components/UsersCheckedIn.dart';
+import 'package:clubbers/app/shared/styles/app_styles.dart';
+import 'package:clubbers/app/shared/styles/typography.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'home_controller.dart';
+import 'widgets/FollowingPlaces.dart';
+import 'widgets/HomeAppBar.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -14,10 +23,67 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      bottomNavigationBar: AppBottomBar(),
+      body: CustomScrollView(
+        slivers: [
+          //
+          // HOME APP BAR
+          //
+          HomeAppBar(),
+          /*
+            CHECKED IN USERS
+            Here we show a ListView with users that have 
+            checked in a specific local. So other users can know
+            that friends are out.
+          */
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(AppStyles.spacing_normal),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("No rolê agora"),
+                  SizedBox(height: AppStyles.spacing_small),
+                  UsersCheckedIn(users: checkedInUsers),
+                ],
+              ),
+            ),
+          ),
+          /*
+            FAVORITE PLACES
+            Here we show a ListView of the favorite places
+            the user is following
+          */
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(AppStyles.spacing_normal),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TEXT(
+                    text: "Locais favoritos",
+                    weight: FontWeight.bold,
+                    size: 18,
+                    noMargin: true,
+                  ),
+                  SizedBox(height: AppStyles.spacing_normal),
+                  FavoritePlaces(places: locals),
+                ],
+              ),
+            ),
+          ),
+
+          /*
+            OTHER PLACES
+            Here we show a ListView of all places
+            the user is following
+          */
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20.0),
+            sliver: FollowingPlaces(places: locals),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
