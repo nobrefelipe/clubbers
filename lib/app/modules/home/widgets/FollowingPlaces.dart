a36613d5c20b48b24c7a8a6d89c3245105b20d5b
Felipe Nobre <felipe@parentscheme.com>
2020-11-23 13:44:22 -0300

    Feat : Removed shadow

--- lib/app/modules/home/widgets/FollowingPlaces.dart
+++ lib/app/modules/home/widgets/FollowingPlaces.dart
@@ -1,8 +1,5 @@
 import 'package:clubbers/app/api/models/local_model.dart';
 import 'package:clubbers/app/shared/components/AspectRatioImage.dart';
-import 'package:clubbers/app/shared/components/ProfileAvatar.dart';
-import 'package:clubbers/app/shared/components/ShadowContainer.dart';
-import 'package:clubbers/app/shared/styles/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
@@ -19,13 +16,10 @@ class FollowingPlaces extends StatelessWidget {
           final Local place = places[index];
           return Container(
             padding: const EdgeInsets.symmetric(vertical: 8.0),
-            margin: const EdgeInsets.only(bottom: AppStyles.spacing_small),
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [
-              BoxShadow(
-                blurRadius: 10,
-                color: Colors.grey[300],
-              )
-            ]),
+            margin: const EdgeInsets.only(bottom: 1.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
             child: ListTile(
               onTap: () => print("sdklmdlsmkdmslkmdklsmk"),
               //
