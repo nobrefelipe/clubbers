1e41a2b19885c0ece628daa1bea279081a6d232f
Felipe Nobre <felipe@parentscheme.com>
2020-11-23 21:20:20 -0300

    Feat : add more events to mock api

--- lib/app/api/mock/events.dart
+++ lib/app/api/mock/events.dart
@@ -2,12 +2,28 @@ import '../models/models.dart';
 
 final List<Event> events = [
   Event(
-    id: "1",
+    id: "0",
     localId: "1",
     // owner: locals[0],
     updatedAt: "",
     createdAt: "",
     name: "Baile do Dennis",
+    price: "189,00",
+    date: "2020-11-25 16:09:57.252407",
+    ticketLink: "https://www.google.com.br",
+    artists: ['Dennis dj', 'Ludmila', 'Kevinho'],
+    description:
+        "The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc. The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc.",
+    banner:
+        'https://cdn-az.allevents.in/events3/banners/45e162f8eb6f0514ab85ab79b2c1d5f29fb25073f6430610f68335a203f6a827-rimg-w526-h275-gmir.jpg',
+  ),
+  Event(
+    id: "1",
+    localId: "1",
+    // owner: locals[0],
+    updatedAt: "",
+    createdAt: "",
+    name: "Gustavo Lima",
     price: "189,00",
     date: "2020-11-25 16:09:57.252407",
     ticketLink: "https://www.google.com.br",
@@ -39,9 +55,41 @@ final List<Event> events = [
     // owner: locals[0],
     updatedAt: "",
     createdAt: "",
-    name: "Astrix",
+    name: "Ace Ventura",
     price: "229,00",
     date: "2020-11-30 16:33:57.252407",
+    ticketLink: "https://www.google.com.br",
+    artists: ["Kevinho"],
+    description:
+        "The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc. The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc.",
+    banner:
+        'https://cdn.zomoofficial.com/wp-content/uploads/2020/08/BANNER-NOTICIA-KEVINHO.png',
+  ),
+  Event(
+    id: "4",
+    localId: "1",
+    // owner: locals[0],
+    updatedAt: "",
+    createdAt: "",
+    name: "Ace Ventura",
+    price: "229,00",
+    date: "2020-11-25 22:33:57.252407",
+    ticketLink: "https://www.google.com.br",
+    artists: ["Kevinho"],
+    description:
+        "The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc. The visual builder allows you to rapidly choose and modify your fields for your custom content. Drag and drop rich text fields, geopoints, images, color fields, relational fields, etc.",
+    banner:
+        'https://cdn.zomoofficial.com/wp-content/uploads/2020/08/BANNER-NOTICIA-KEVINHO.png',
+  ),
+  Event(
+    id: "6",
+    localId: "1",
+    // owner: locals[0],
+    updatedAt: "",
+    createdAt: "",
+    name: "Ace Ventura",
+    price: "229,00",
+    date: "2020-12-30 19:33:57.252407",
     ticketLink: "https://www.google.com.br",
     artists: ["Kevinho"],
     description:
